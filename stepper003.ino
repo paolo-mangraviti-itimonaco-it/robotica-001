1a9f85df7ba797899a38cd3d7ecbe4f55a665cd2
paolo-mangraviti-itimonaco-it <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2024-11-12 08:40:00 +0100

    Update stepper003.ino
    .

--- stepper003.ino
+++ stepper003.ino
@@ -6,6 +6,7 @@ const int ORARIO = 1;
 const int ANTIORARIO = -1;
 // definizione del numero di passi per rotazione - 32 passi per rotazione ma con riduzione 1/64 (circa) (1/63,68395 = )
 const int passiPerRotazione = 2048;
+const int numeroMotori = 1;
 const int velocita = 40;
 const int primoPin[] = {4};   // Collegamenti ai controller ULN2003
 int registro[] = {1};         // registri motori
@@ -37,10 +38,12 @@ void eseguiPasso(int senso,int motore)
   if (registro[motore] > 8) registro[motore]=1;
   if (registro[motore] <=0) registro[motore] = 8;
 }
-void setup() {
 
-// inizializzazione della porta seriale
+void inizializzaMotori(int motore) { for (int i=0; i<4; i++) { pinMode(primoPin[motore]+i,OUTPUT); } }
+
+void setup() {
   Serial.begin(9600);
+  for (int m = 0; m<numeroMotori; m++) { inizializzaMotori(m); }
 }
 
 void loop() {
